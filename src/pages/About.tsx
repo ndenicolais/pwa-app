c978e578ace78446b90ab7dd53101042a01160ac
ndenicolais <n.denicolais@arethusasrl.it>
2022-05-20 17:09:04 +0200

    Update_Version

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,7 +4,7 @@ function About() {
     return (
         <div className='About'>
         <h1>ABOUT</h1>
-        <h2>Demo creata con l'utilizzo di React</h2>
+        <h2>Demo creata con l'utilizzo di Vite.js, React TypeScript e React Router</h2>
         <br/>
         <form action='/'>
         <button id="default-button"
--- src/pages/Contatti.tsx
+++ src/pages/Contatti.tsx
@@ -4,19 +4,12 @@ function Contatti() {
   return (
     <div className="Servizi">
       <h1>CONTATTI</h1>
-      <dt><b>Arethusa Srl:<br/></b></dt>
+      <dt><b>:<br/></b></dt>
       <h2>
-      <dt>Tel: +39 081 7577738</dt>
-        <dt>Fax: +39 081 7576215</dt>
-          <dt>Web: www.arethusasrl.it</dt>
-            <dt>Email: arethusa@arethusasrl.it</dt>
-          </h2><br/>
-      <dt><b>Natura Srl:<br/></b></dt>
-      <h2>
-      <dt>Tel: +39 081 5737038</dt>
-        <dt>Fax: +39 081 5739776</dt>
-          <dt>Web: www.naturasrl.it</dt>
-            <dt>Email: natura@naturasrl.it</dt>
+      <dt>Tel: </dt>
+        <dt>Fax: </dt>
+          <dt>Web: </dt>
+            <dt>Email: </dt>
           </h2><br/>
       <form action="/">
       <button id="default-button"
