19b524c770a05064f54317fd5b318cbc577ffb3e
ndenicolais <n.denicolais@arethusasrl.it>
2022-06-20 10:15:38 +0200

    Update

--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -3,7 +3,7 @@ import '/src/styles/Navbar.css';
 
 function Navbar() {
     return (
-        <div className='navbar'>
+        <div className='Navbar'>
             <ul>
                 <li>
                 <Link to="/">HOME</Link>
